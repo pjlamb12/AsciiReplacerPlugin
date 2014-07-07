103ea98d8b222fbb12bca35f6aee16b881e50657
Preston Lamb <preston.j.lamb@gmail.com>
2014-07-07 11:19:27 -0600

    changed the plugin's name to AsciiKicker

---
+++ README.md
@@ -0,0 +1,13 @@
+# Replacer Plugin for Sublime Text
+This is a simple plugin that takes apostrophes, quotation marks, and ampersands and replaces them with the plain text or html code version of them. Run this so that all those characters show up properly in your file.
+
+##Using it
+Once installing through Package Control, you can find Replacer in the "Edit" menu in Sublime. Also, it is set as a default for a key binding:
+
+Windows and Linus: `ctrl+alt+r`
+
+Mac: `ctrl+super+r`
+
+Remember that in Mac, super = command
+
+Highlight the text you want to check for these invalid characters, then use the shortcut or the Edit menu.
---
+++ ascii_kicker.py
@@ -0,0 +1,19 @@
+import sublime, sublime_plugin, string
+
+class AsciiKickerCommand(sublime_plugin.TextCommand):
+	
+	def run(self, edit):
+		for r in reversed(self.view.sel()):
+			for line_r in reversed(self.view.lines(r)):
+				text = self.view.substr(line_r)
+				print(text)
+				text = text.replace('& ', '&amp; ')
+				text = text.replace("‘", "'")
+				text = text.replace("’", "'")
+				text = text.replace('“', '"')
+				text = text.replace('”', '"')
+				text = text.replace('% ', '&#37; ')
+				text = text.replace('…', '&#8230;')
+				self.view.replace(edit, line_r, text)
+		self.view.erase_status('ascii_kicker')
+		sublime.status_message('Ascii Kicker is finished running!')
